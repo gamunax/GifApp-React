c87491008505942c3f667ef5235869274472a664
Jan Pierre <gamunaxx@gmail.com>
2020-08-03 19:06:46 -0500

    feat: add component item

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
   const [count, setCount] = useState(0);
+  const [images, setimages] = useState([]);
 
   useEffect( () => {
     getGifs();
@@ -23,13 +25,21 @@ export const GifGrid = ({ category }) => {
     })
 
     console.log(gifs);
+    setimages(gifs);
   }
 
   return (
     <>
       <h3>{ category }</h3>
-      <h3>{ count }</h3>
-      <button onClick={ () => setCount(count + 1)}></button>
+      
+      {
+        images.map(img => (
+          <GifGridItem 
+            key={img.id}
+            {...img}></GifGridItem>
+        ))
+      }
+      
     </>
   )
 }
